a828fbbe532bef569995875c2848ebb36b8bc941
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2025-04-14 19:23:33 -0300

    email in footer working

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ const Footer: React.FC = () => {
         <footer className="w-full py-4 px-6 mt-10 border-t dark:border-gray-700 flex flex-col items-center text-sm text-center">
         <p className="mb-2">&copy; {new Date().getFullYear()} {t('madeby')} Guilherme Gomes Luccas Rodrigues</p>
         <div className="flex gap-4">
-            <a href="guilhermegomes.luccas@gmail.com" className="hover:underline text-blue-500 dark:text-blue-400">
-            Email
+            <a href="mailto:guilhermegomes.luccas@gmail.com?subject=Contato&body=Olá Guilherme," className="hover:underline text-blue-500 dark:text-blue-400">
+                Email
             </a>
             <a href="https://www.linkedin.com/in/guilherme-gomes-luccas/" target="_blank" rel="noopener noreferrer" className="hover:underline text-blue-500 dark:text-blue-400">
-            LinkedIn
+                LinkedIn
             </a>
             <a href="" target="_blank" rel="noopener noreferrer" className="hover:underline text-blue-500 dark:text-blue-400">
-            Currículo ({t('soon')})
+                Currículo ({t('soon')})
             </a>
         </div>
         </footer>
